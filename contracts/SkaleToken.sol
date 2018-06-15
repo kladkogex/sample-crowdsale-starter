3e360696a0faaeffae0d1e5517a03a9f0b36f22a
Konstantin Kladko <you@example.com>
2018-06-15 17:00:01 +0300

    Done

--- contracts/SkaleToken.sol
+++ contracts/SkaleToken.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.4.18;
 
 import "zeppelin-solidity/contracts/token/ERC20/MintableToken.sol";
 
-contract MyToken is MintableToken {
+contract SkaleToken is MintableToken {
   string public constant name = "My Token";
   string public constant symbol = "MYT";
   uint8 public constant decimals = 18;
